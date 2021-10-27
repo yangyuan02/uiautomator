e86ecc9a2b7c9d150af9e6721fbc373ccee230e1
yangyuan <1367511704@qq.com>
2021-10-28 01:54:15 +0800

    更新chrome types类型

---
+++ src/content_script/a.ts
@@ -0,0 +1,13 @@
+class a {
+    public static async sendMessage(message: Command<any>): Promise<any> {
+        return new Promise((resolve, reject) => {
+            chrome.runtime.sendMessage(message, (result: CommandExecResult) => {
+                if (result.isSuccess) {
+                    return resolve(result.payload);
+                } else {
+                    return reject(result.payload);
+                }
+            });
+        });
+    }
+}
